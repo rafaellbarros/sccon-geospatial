480797babc4e7419620f15a698f611fedcb0731c
Rafael Barros <rafaelbarros.df@gmail.com>
2026-04-01 16:22:24 -0300

    🎉 initial: initialize project with spring-boot-starter-web, spring-boot-starter-data-jpa, h2 and lombok

---
+++ src/main/java/br/com/rafaellbarros/sccon/geospatial/ScconGeospatialApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaellbarros.sccon.geospatial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScconGeospatialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScconGeospatialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaellbarros/sccon/geospatial/ScconGeospatialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaellbarros.sccon.geospatial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScconGeospatialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
